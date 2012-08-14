1e4ba66ae4af07b8efd42a3ce420cb9fabc6a18d
Danny <danny.hawkins@slotpartners.com>
2012-08-14 21:05:33 +0300

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tictac::Application.config.secret_token = 'c5e0a14ab70829898feeca6d1c83ff86b674670aefca75a8df1cb4a061022f0cf71dff7db26aad95299f45fa758fa7538d04fc9dd274e06ad4a2219c5417978c'
