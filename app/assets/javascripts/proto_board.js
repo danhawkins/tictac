07b4dc1e8f090bb5d142b6cb9d38028455abe536
Danny <danny.hawkins@slotpartners.com>
2012-08-15 09:50:51 +0300

    updated logic

--- app/assets/javascripts/proto_board.js
+++ app/assets/javascripts/proto_board.js
@@ -1,19 +1,44 @@
 var ProtoBoard = {
 	combinations: [ ['0_0','0_1','0_2'], ['1_0','1_1','1_2'], ['2_0','2_1','2_2'],['0_0','1_0','2_0'],['0_1','1_1','2_1'],['0_2','1_2','2_2'],['0_0','1_1','2_2'],['0_2','1_1','2_0']],
-	blank: 0,
-	human: 1,
-	computer: 2,
-	current_game: [],
+	blank: '',
+	human: 'X',
+	computer: 'O',
+	human_cells: [],
+	computer_cells: [],
+	computer_moved: false,
 
 	init: function(){
-		this.current_game = [];
-		this.bindCells();
+
+		ProtoBoard.current_game = [];
+		ProtoBoard.bindCells();
 	},
 
 	bindCells: function(){
 		$('#gameboard .cell').live('click',function(){
-			$(this).text('o');
-		})
+			ProtoBoard.human_cells.push($(this).attr('id'));
+
+			$(this).text(ProtoBoard.human);
+			console.log(ProtoBoard.current_game);
+
+			ProtoBoard.computer_moved = false;
+			ProtoBoard.computerMove();
+		});
+	},
+
+	computerMove: function(){
+
+	},
+
+	checkForWin: function(){
+
+	},
+
+	blockHumanWin: function(){
+
+	},
+
+	placeRandom: function(){
+
 	}
 
 };
